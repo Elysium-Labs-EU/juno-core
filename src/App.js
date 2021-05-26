8cb7809efeceaa7398c01d2b193440b03d350933
Robbert Tuerlings <robberttg@protonmail.com>
2021-05-26 09:43:17 +0200

    Initial setup

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ import Settings from './components/Settings'
 import InformationOverview from './components/InformationOverview'
 import FileOverview from './components/FileOverview'
 import Inbox from './components/Inbox/Inbox'
+import SpamEmail from './components/Spam/Spam'
 import Header from './components/MainHeader/Header'
-
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import DraftEmail from './components/Draft/DraftEmail'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 const mapStateToProps = (state) => {
   const { baseLoaded } = state
@@ -39,6 +39,7 @@ const App = ({ baseLoaded, dispatch }) => {
           <Route path="/mail/:labelId/:threadId" component={EmailDetail} />
           <Route path="/compose" component={ComposeEmail} />
           <Route path="/drafts" component={DraftEmail} />
+          <Route path="/spam" component={SpamEmail} />
           <Route path="/settings" component={Settings} />
           <Route path="/information-overview" component={InformationOverview} />
           <Route path="/file-overview" component={FileOverview} />
