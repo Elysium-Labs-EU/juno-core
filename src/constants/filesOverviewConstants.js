84c3b57fd20fa8d6661cc11611f232f2901f1529
Robbert Tuerlings <robberttg@protonmail.com>
2021-07-13 13:33:32 +0200

    Update filesOverviewConstants.js

--- src/constants/filesOverviewConstants.js
+++ src/constants/filesOverviewConstants.js
@@ -1,2 +1,3 @@
 /* eslint-disable import/prefer-default-export */
-export const NO_FILES = 'This email has no files.'
+export const NO_FILES =
+  "There aren’t any files to see here right now. But there could be — once this email's messages will have attachments this will be added here."
