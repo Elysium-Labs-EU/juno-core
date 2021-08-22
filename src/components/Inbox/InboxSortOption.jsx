c13c99d0fe3e5dc2888bb0a22378373b44351b42
Robbert Tuerlings <robberttg@protonmail.com>
2021-08-22 14:32:04 +0200

    Reconnect startSort function to EmailList
    Changed metaList for emailList redux state

--- src/components/Inbox/InboxSortOption.jsx
+++ src/components/Inbox/InboxSortOption.jsx
@@ -4,21 +4,17 @@ import { useHistory } from 'react-router-dom'
 import { MdRefresh } from 'react-icons/md'
 import { CustomButtonText, CustomIconLink } from '../Elements/Buttons'
 import { convertArrayToString } from '../../utils'
-import {
-  refreshEmailFeed,
-  selectIsFetching,
-  selectMetaList,
-} from '../../Store/metaListSlice'
+import { refreshEmailFeed, selectIsFetching } from '../../Store/metaListSlice'
 import { selectLabelIds } from '../../Store/labelsSlice'
 import { selectIsLoading } from '../../Store/utilsSlice'
 import * as S from './InboxSortOptionStyles'
 import startSort from '../../utils/startSort'
-import { setIsSorting } from '../../Store/emailListSlice'
+import { selectEmailList, setIsSorting } from '../../Store/emailListSlice'
 
 const INBOX_BUTTON = 'Sort inbox'
 
 const SortInbox = () => {
-  const metaList = useSelector(selectMetaList)
+  const emailList = useSelector(selectEmailList)
   const labelIds = useSelector(selectLabelIds)
   const isLoading = useSelector(selectIsLoading)
   const isFetching = useSelector(selectIsFetching)
@@ -26,17 +22,17 @@ const SortInbox = () => {
   const history = useHistory()
   const [disableRefresh, setDisableRefresh] = useState(false)
 
-  const metaListIndex = useMemo(
+  const emailListIndex = useMemo(
     () =>
-      metaList.findIndex((threadList) =>
+      emailList.findIndex((threadList) =>
         threadList.labels.includes(...labelIds)
       ),
-    [metaList, labelIds]
+    [emailList, labelIds]
   )
 
   const handleClick = () => {
     const labelURL = convertArrayToString(labelIds && labelIds)
-    startSort({ history, labelURL, metaList, metaListIndex })
+    startSort({ history, labelURL, emailList, emailListIndex })
     dispatch(setIsSorting(true))
   }
 
--- src/components/ToDo/TodoFocusOption.jsx
+++ src/components/ToDo/TodoFocusOption.jsx
@@ -4,31 +4,30 @@ import { useHistory } from 'react-router-dom'
 import { CustomButtonText } from '../Elements/Buttons'
 import { convertArrayToString } from '../../utils'
 import { selectLabelIds } from '../../Store/labelsSlice'
-import { selectMetaList } from '../../Store/metaListSlice'
 import { selectIsLoading } from '../../Store/utilsSlice'
 import * as S from './TodoFocusOptionStyles'
 import * as local from '../../constants/todoConstants'
 import startSort from '../../utils/startSort'
-import { setIsFocused } from '../../Store/emailListSlice'
+import { selectEmailList, setIsFocused } from '../../Store/emailListSlice'
 
 const TodoFocusOption = () => {
   const labelIds = useSelector(selectLabelIds)
   const isLoading = useSelector(selectIsLoading)
-  const metaList = useSelector(selectMetaList)
+  const emailList = useSelector(selectEmailList)
   const history = useHistory()
   const dispatch = useDispatch()
 
-  const metaListIndex = useMemo(
+  const emailListIndex = useMemo(
     () =>
-      metaList.findIndex((threadList) =>
+      emailList.findIndex((threadList) =>
         threadList.labels.includes(...labelIds)
       ),
-    [metaList, labelIds]
+    [emailList, labelIds]
   )
 
   const handleClick = () => {
     const labelURL = convertArrayToString(labelIds && labelIds)
-    startSort({ history, labelURL, metaList, metaListIndex })
+    startSort({ history, labelURL, emailList, emailListIndex })
     dispatch(setIsFocused(true))
   }
 
