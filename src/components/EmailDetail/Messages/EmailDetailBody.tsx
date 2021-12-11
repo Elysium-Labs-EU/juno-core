8a9f05096b4ed86315ae7496ebfeda0ee671efd5
Robbert <robberttg@protonmail.com>
2021-12-11 10:33:16 +0100

    Change decoding

--- src/components/EmailDetail/Messages/EmailDetailBody.tsx
+++ src/components/EmailDetail/Messages/EmailDetailBody.tsx
@@ -39,86 +39,47 @@ const EmailDetailBody = ({
   // This function recursively loops in the emailbody to find a body to decode.
   const bodyDecoder = (inputObject: any) => {
     Object.keys(inputObject).forEach((key) => {
-      // console.log(messageId, inputObject)
-      if (inputObject.body.size > 0) {
-        if (key === 'body') {
-          // console.log(key)
-          // console.log(inputObject.body)
-          if (Object.prototype.hasOwnProperty.call(inputObject.body, 'attachmentId')) {
-            inlineImage(inputObject)
-          }
-          const str = decodeBase64(`${ inputObject.body.data }`)
-          // console.log(str)
-          if (str) setBodyState((prevState) => [...prevState, str])
-        }
-      }
-      if (inputObject.body.size === 0) {
-        if (key === 'parts') {
-          if (inputObject.parts[0].body.size > 0 && inputObject.parts[1].body.size > 0) {
-            console.log('here1', inputObject)
-            // Prioritise part 1 due to HTML content
-            bodyDecoder(inputObject.parts[1])
-          }
-          if (inputObject.parts[0].body.size === 0 && inputObject.parts[1].body.size > 0) {
-            console.log('here2', inputObject)
-            bodyDecoder(inputObject.parts[1])
-            // If the body size is 0, verify if it is an attachment
-            if (Object.prototype.hasOwnProperty.call(inputObject.parts[0], 'parts')) {
-              bodyDecoder(inputObject.parts[0])
+      if (key === 'body' || key === 'parts') {
+        console.log(inputObject)
+        if (inputObject.body.size > 0) {
+          if (key === 'body') {
+            // console.log(key)
+            console.log(inputObject)
+            if (Object.prototype.hasOwnProperty.call(inputObject.body, 'attachmentId')) {
+              inlineImage(inputObject)
             }
+            const str = decodeBase64(`${ inputObject.body.data }`)
+            // console.log(str)
+            if (str) setBodyState((prevState) => [...prevState, str])
           }
-          if (inputObject.parts[0].body.size > 0 && inputObject.parts[1].body.size === 0) {
-            console.log('here3', inputObject)
-            bodyDecoder(inputObject.parts[0])
-            // If the body size is 0, verify if it is an attachment
+        }
+        if (inputObject.body.size === 0 || !Object.prototype.hasOwnProperty.call(inputObject, 'body')) {
+          if (key === 'parts') {
+            console.log(inputObject.parts)
+            if (Object.prototype.hasOwnProperty.call(inputObject.parts[0], 'parts')) {
+              console.log('here1')
+              bodyDecoder(inputObject.parts[0])
+              if (Object.prototype.hasOwnProperty.call(inputObject.parts[1].body, 'attachmentId')) {
+                bodyDecoder(inputObject.parts[1])
+              }
+              return
+            }
             if (Object.prototype.hasOwnProperty.call(inputObject.parts[1], 'parts')) {
+              console.log('here2')
+              bodyDecoder(inputObject.parts[1])
+              return
+            }
+            if (Object.prototype.hasOwnProperty.call(inputObject.parts[1].body, 'attachmentId')) {
+              console.log('here3')
+              console.log(inputObject.parts[0])
+              console.log(inputObject.parts[1])
+              bodyDecoder(inputObject.parts[0])
+              bodyDecoder(inputObject.parts[1])
+            }
+            if (Object.prototype.hasOwnProperty.call(inputObject.parts[1], 'body')) {
               bodyDecoder(inputObject.parts[1])
             }
           }
-          if (inputObject.parts[0].body.size === 0 && inputObject.parts[1].body.size === 0) {
-            console.log('here4', inputObject)
-            // Check if the part is an attachment
-            inputObject.parts.forEach((part: any) => {
-              if (Object.prototype.hasOwnProperty.call(part.body, 'attachmentId')) {
-                bodyDecoder(part)
-              }
-            })
-          }
-        }
-        if (key === 'attachmentId') {
-          console.log('attachmentFound1')
-          // if (inputObject.parts[0].body.size > 0) {
-          // const str = decodeBase64(
-          //   `${ inputObject.parts[0].body.data }`
-          // )
-          // if (str) setBodyState((prevState) => [...prevState, str])
-          // }
-          // if (inputObject.parts[0].body.size === 0) {
-          //   bodyDecoder(inputObject.parts[0])
-          // }
-
-          // If there is no body on this level, loop through it again
-          // if (inputObject.parts[inputObject.parts.length - 1].body.size === 0) {
-          //   console.log('here')
-          //   bodyDecoder(inputObject.parts[inputObject.parts.length - 1])
-          // }
-
-          // If there is a body on this level, decode it
-          // if (inputObject.parts[inputObject.parts.length - 1].body.size > 0) {
-          //   console.log('here2')
-          //   if (inputObject.parts[inputObject.parts.length - 1].filename.length === 0) {
-          //     const str = decodeBase64(
-          //       `${ inputObject.parts[inputObject.parts.length - 1].body.data }`
-          //     )
-          //     // console.log(str)
-          //     if (str) setBodyState((prevState) => [...prevState, str])
-          //     bodyDecoder(inputObject.parts[inputObject.parts.length - 1])
-          //   }
-          //   if (inputObject.parts[inputObject.parts.length - 1].filename.length > 0) {
-          //     inlineImage()
-          //     bodyDecoder(inputObject.parts[inputObject.parts.length - 1])
-          //   }
-          // }
         }
       }
     })
