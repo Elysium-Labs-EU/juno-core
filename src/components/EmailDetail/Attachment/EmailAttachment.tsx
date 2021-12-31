0d961acfab288e22b5879d8b59814e33193246a7
Robbert <robberttg@protonmail.com>
2021-12-31 13:22:11 +0100

    Clean up

--- src/components/EmailDetail/Attachment/EmailAttachment.tsx
+++ src/components/EmailDetail/Attachment/EmailAttachment.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import isEmpty from 'lodash/isEmpty'
 import EmailAttachmentBubble from './EmailAttachmentBubble'
-import * as fileOverview from '../../../constants/filesOverviewConstants'
 import * as S from './EmailAttachmentStyles'
 import { EmailMessage } from '../../../Store/emailListTypes'
 import { IEmailAttachmentType } from './EmailAttachmentTypes'
 
-const EmailAttachment = ({ message, overview }: { message: EmailMessage; overview: boolean }) => {
+const EmailAttachment = ({ message }: { message: EmailMessage }) => {
   const CheckAttachment = () => {
     if (
       message &&
@@ -25,7 +24,7 @@ const EmailAttachment = ({ message, overview }: { message: EmailMessage; overvie
           ) : null
         )
       }
-      return overview ? fileOverview.NO_FILES : null
+      return null
     }
     return null
   }
