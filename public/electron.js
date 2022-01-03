d812b23a643f207ae53e4f4de9b46cac026f700c
Robbert <robberttg@protonmail.com>
2022-01-03 14:21:18 +0100

    Cleanup + windows changes

--- public/electron.js
+++ public/electron.js
@@ -6,8 +6,8 @@ const url = require('url')
 // Create the native browser window.
 function createWindow() {
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1280,
+    height: 800,
     // Set the path of an additional "preload" script that can be used to
     // communicate between node-land and browser-land.
     webPreferences: {
