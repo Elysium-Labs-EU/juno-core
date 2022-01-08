4872c05e627d74f429e0ed53d501aee848b03433
Robbert <robberttg@protonmail.com>
2022-01-08 12:56:18 +0100

    Update TimeStampDisplay.tsx
    Removed commented import
    Removed unused variable
    Change type declaration to interface (these are faster)

--- src/components/Elements/TimeStamp/TimeStampDisplay.tsx
+++ src/components/Elements/TimeStamp/TimeStampDisplay.tsx
@@ -1,23 +1,24 @@
 import React from 'react'
 import { format, isSameDay, isThisYear } from 'date-fns'
-// import { isThisYear } from 'date-fns/esm'
 
-type TimeStampType = {
+interface IThreadTimeStamp {
   threadTimeStamp: string
 }
 
-const TimeStampDisplay = ({ threadTimeStamp }: TimeStampType) => {
+const TimeStampDisplay = ({ threadTimeStamp }: IThreadTimeStamp) => {
   const unixTimestamp = parseInt(threadTimeStamp.toString(), 10)
   const currentTimestamp = Date.now()
-  const currentYear = Date.now()
 
-  // If the timestamp is of today - send hours, if timestamp is not of today send date
-  if(isThisYear(unixTimestamp)){
+  // If the timestamp is of today - send hours, 
+  // If timestamp is not of today send date without year,
+  // If timestamp if from another year, show full date.
+
+  if (isThisYear(unixTimestamp)) {
     const isSameDayCheck = isSameDay(currentTimestamp, unixTimestamp) ?
       format(unixTimestamp, 'HH:mm') : format(unixTimestamp, 'dd LLL')
-      return <span className="date">{isSameDayCheck}</span>
-  } 
-  
+    return <span className="date">{isSameDayCheck}</span>
+  }
+
   return <span className="date">{format(unixTimestamp, 'dd LLL yyyy')}</span>
 }
 
