af38a291584a779c60657c3795d101e7e3a42faf
Abhishek-90 <holaniabhishek90@gmail.com>
2022-01-14 23:49:01 +0530

    Resolved counter issue

--- src/components/EmailDetail/EmailPosition/EmailPosition.tsx
+++ src/components/EmailDetail/EmailPosition/EmailPosition.tsx
@@ -1,18 +1,23 @@
 import React, { useEffect, useRef } from 'react'
+import { selectViewIndex } from '../../../Store/emailDetailSlice'
 import { IEmailListObject } from '../../../Store/emailListTypes'
+import { useAppSelector } from '../../../Store/hooks'
 import * as S from './EmailPositionStyles'
 
 const EmailPosition = ({ activeEmailList }: { activeEmailList: IEmailListObject }) => {
     const threadsObjectStart = useRef<IEmailListObject | undefined>()
+    const viewIndex = useAppSelector(selectViewIndex)
 
     useEffect(() => {
-        threadsObjectStart.current = activeEmailList
-    }, [])
+        if((threadsObjectStart.current === undefined) || (threadsObjectStart.current && threadsObjectStart.current.threads.length < activeEmailList.threads.length)){
+            threadsObjectStart.current = activeEmailList
+        }
+    }, [activeEmailList])
 
     return (
         <S.Wrapper>
             {threadsObjectStart.current &&
-                <p className="text_muted">{threadsObjectStart.current.threads.length - activeEmailList.threads.length + 1} / {threadsObjectStart.current.threads.length}</p>}
+                <p className="text_muted">{viewIndex + 1} / {threadsObjectStart.current.threads.length}</p>}
         </S.Wrapper>
     )
 }
