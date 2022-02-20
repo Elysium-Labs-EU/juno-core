0c19f5ed63041ba07e9b1e789de60343636e5b49
Robbert <robberttg@protonmail.com>
2022-02-20 17:17:53 +0100

    Setup sentry source mapping

--- src/index.tsx
+++ src/index.tsx
@@ -10,16 +10,17 @@ import reportWebVitals from './reportWebVitals'
 import { GlobalStyle, theme } from './styles/globalStyles'
 
 // Don't run Sentry when developing.
-process.env.NODE_ENV !== 'development' &&
+if (process.env.NODE_ENV === 'production' && process.env.REACT_APP_SENTRY_RELEASE) {
   Sentry.init({
-    dsn: 'https://493389e033b54228a97252c427fdebe6@o917516.ingest.sentry.io/5859846',
+    dsn: process.env.REACT_APP_SENTRY_DSN,
     integrations: [new Integrations.BrowserTracing()],
-
+    release: process.env.REACT_APP_SENTRY_RELEASE,
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
     // We recommend adjusting this value in production
     tracesSampleRate: 1.0,
   })
+}
 
 ReactDOM.render(
   <StrictMode>
