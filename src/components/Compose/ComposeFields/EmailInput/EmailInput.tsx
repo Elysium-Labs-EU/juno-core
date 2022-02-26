d776344ca4f50e3e3445660f8378be8dcabfc7d6
Robbert <robberttg@protonmail.com>
2022-02-26 10:02:17 +0100

    Update contact listing
    Only fetch new resources if the initial result is 0 or after 1 second.

--- src/components/Compose/ComposeFields/EmailInput/EmailInput.tsx
+++ src/components/Compose/ComposeFields/EmailInput/EmailInput.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { useEffect, useState } from 'react'
 import Autocomplete from '@mui/material/Autocomplete'
+import { matchSorter } from 'match-sorter'
 import StyledTextField from './EmailInputStyles'
 import RecipientChip from '../../../Elements/RecipientChip/RecipientChip'
 import { Contact } from '../../../../Store/contactsTypes'
@@ -14,6 +15,7 @@ import {
 import contactApi from '../../../../data/contactApi'
 import { setServiceUnavailable } from '../../../../Store/utilsSlice'
 import useDebounce from '../../../../Hooks/useDebounce'
+import emailValidation from '../../../../utils/emailValidation'
 
 interface IEmailInputProps {
   id: string
@@ -30,6 +32,57 @@ interface IHandleIncompleteInput {
   inputValue: string
 }
 
+interface IFetchContacts {
+  inputValue: string
+  dispatch: Function
+  setCompletedSearch: Function
+}
+
+const fetchContacts = async ({
+  inputValue,
+  dispatch,
+  setCompletedSearch,
+}: IFetchContacts) => {
+  const params = {
+    query: inputValue,
+    readMask: 'emailAddresses,names',
+  }
+  try {
+    const responseQueryContacts = await contactApi().queryContacts(params)
+    if (responseQueryContacts.status === 200) {
+      const {
+        data: { results },
+      } = responseQueryContacts
+
+      const mappedResults =
+        results && results.length > 0
+          ? results.map(
+              (contact: any): Contact => ({
+                name: Object.prototype.hasOwnProperty.call(
+                  contact.person,
+                  'names'
+                )
+                  ? contact.person.names[0].displayName
+                  : contact.person.emailAddresses[0].value,
+                emailAddress: contact.person.emailAddresses[0].value,
+              })
+            )
+          : []
+
+      dispatch(setAllContacts(mappedResults))
+      dispatch(setContactsLoaded(JSON.stringify(Date.now())))
+      setCompletedSearch(true)
+    }
+  } catch (err) {
+    dispatch(setServiceUnavailable('Error fetching contacts.'))
+  }
+}
+
+const filterOptions = (
+  options: Contact[],
+  { inputValue }: { inputValue: string }
+) => matchSorter(options, inputValue, { keys: ['name', 'emailAddress'] })
+
 const emailInput = (props: IEmailInputProps) => {
   const {
     id,
@@ -42,65 +95,49 @@ const emailInput = (props: IEmailInputProps) => {
   } = props
   const [open, setOpen] = useState(false)
   const [options, setOptions] = useState<readonly Contact[]>([])
-  const debouncedInputValue = useDebounce(inputValue, 500)
+  const [completedSearch, setCompletedSearch] = useState(false)
+  const debouncedInputValue: string = useDebounce(inputValue, 500)
   const availableContacts: Contact[] = useAppSelector(selectAllContacts)
   const contactsLoaded: string = useAppSelector(selectContactsLoaded)
   const dispatch = useAppDispatch()
 
+  // Wait for fetching new results, if the current query already has some results. Only search for more after 1 sec.
   useEffect(() => {
     let mounted = true
-    if (debouncedInputValue && debouncedInputValue.length > 1) {
+    if (
+      debouncedInputValue &&
+      debouncedInputValue.length > 1 &&
+      !completedSearch
+    ) {
       ;(async () => {
-        const check = availableContacts.some((contact) =>
-          contact.emailAddress.includes(inputValue.toLowerCase())
-        )
-        if (!check) {
-          const params = {
-            query: inputValue,
-            readMask: 'emailAddresses,names',
-          }
-          try {
-            const responseQueryContacts = await contactApi().queryContacts(
-              params
-            )
-            if (responseQueryContacts.status === 200) {
-              const {
-                data: { results },
-              } = responseQueryContacts
-
-              const mappedResults =
-                results && results.length > 0
-                  ? results.map(
-                      (contact: any): Contact => ({
-                        name: Object.prototype.hasOwnProperty.call(
-                          contact.person,
-                          'names'
-                        )
-                          ? contact.person.names[0].displayName
-                          : contact.person.emailAddresses[0].value,
-                        emailAddress: contact.person.emailAddresses[0].value,
-                      })
-                    )
-                  : []
-
-              dispatch(setAllContacts(mappedResults))
-              dispatch(setContactsLoaded(JSON.stringify(Date.now())))
-            }
-          } catch (err) {
-            dispatch(setServiceUnavailable('Error fetching contacts.'))
-          }
+        const foundResults = filterOptions(availableContacts, {
+          inputValue: debouncedInputValue,
+        })
+        if (foundResults.length === 0) {
+          fetchContacts({ dispatch, inputValue, setCompletedSearch })
         }
-
-        if (mounted) {
-          setOptions(availableContacts)
+        if (foundResults.length > 0) {
+          setTimeout(
+            () => fetchContacts({ dispatch, inputValue, setCompletedSearch }),
+            1000
+          )
         }
       })()
+    }
+    if (mounted) {
+      setOptions(availableContacts)
     }
     return () => {
       mounted = false
     }
-  }, [])
-  // }, [debouncedInputValue, contactsLoaded])
+  }, [debouncedInputValue, contactsLoaded, completedSearch])
+
+  // Only attempt a fetch for details when the user changes the input again
+  useEffect(() => {
+    if (completedSearch) {
+      setCompletedSearch(false)
+    }
+  }, [debouncedInputValue])
 
   useEffect(() => {
     if (!open) {
@@ -108,11 +145,20 @@ const emailInput = (props: IEmailInputProps) => {
     }
   }, [open])
 
+  // If user changes focus and there is incomplete input, attempt to complete it.
   const handleIncompleteInput = (incompleteProps: IHandleIncompleteInput) => {
-    handleChange({
-      newValue: [...valueState, incompleteProps.inputValue],
-      fieldId: incompleteProps.id,
-    })
+    const validation = emailValidation([
+      {
+        name: incompleteProps.inputValue,
+        emailAddress: incompleteProps.inputValue,
+      },
+    ])
+    if (validation) {
+      handleChange({
+        newValue: [...valueState, incompleteProps.inputValue],
+        fieldId: incompleteProps.id,
+      })
+    }
   }
 
   // Clear input when a new contact chip is created.
@@ -122,6 +168,8 @@ const emailInput = (props: IEmailInputProps) => {
 
   return (
     <Autocomplete
+      filterOptions={filterOptions}
+      autoHighlight
       multiple
       limitTags={3}
       id={id}
