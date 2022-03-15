35624c894cf1c6ad99769255bb6e3eee8c20d032
Robbert <robberttg@protonmail.com>
2022-03-15 11:35:51 +0100

    Setup husky

---
+++ lint-staged.config.mjs
@@ -0,0 +1,21 @@
+import { ESLint } from 'eslint'
+
+const removeIgnoredFiles = async (files) => {
+  const eslint = new ESLint()
+  const isIgnored = await Promise.all(
+    files.map((file) => {
+      return eslint.isPathIgnored(file)
+    })
+  )
+  const filteredFiles = files.filter((_, i) => !isIgnored[i])
+  return filteredFiles.join(' ')
+}
+
+export default {
+  '*.{js,jsx,ts,tsx}': async (files) => {
+    const filesToLint = await removeIgnoredFiles(files)
+    return [`eslint --max-warnings=0 ${filesToLint}`]
+  },
+  '*.{js,jsx,ts,tsx,json,css}': ['prettier --write'],
+  '*.{ts,tsx}': ['tsc-files --noEmit'],
+}
