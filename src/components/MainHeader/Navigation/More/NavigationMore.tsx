9267d26ff55ad782940683e9e6c10d2e89dbebb0
Robbert <robberttg@protonmail.com>
2022-03-19 19:51:19 +0100

    Fix positioning menu

--- src/components/MainHeader/Navigation/More/NavigationMore.tsx
+++ src/components/MainHeader/Navigation/More/NavigationMore.tsx
@@ -1,22 +1,40 @@
+import { Divider, MenuItem } from '@mui/material'
+import * as GS from '../../../../styles/globalStyles'
 import * as S from './NavigationMoreStyles'
 import * as local from '../../../../constants/subMenuHeaderConstants'
 import CustomLink from '../../../Elements/Links/CustomLink'
 import SettingsOption from './Options/SettingsOption'
 import LogoutOption from './Options/LogoutOption'
 
-const NavigationMore = () => (
-  <S.Wrapper>
-    <S.TopMenu>
-      {local.MENU_OPTIONS &&
-        local.MENU_OPTIONS.map((item, index) => (
-          <S.InnerMenu key={`${item.name + index}`}>
-            <CustomLink to={item.link} label={item.name} />
-          </S.InnerMenu>
-        ))}
-    </S.TopMenu>
-    <SettingsOption />
-    <LogoutOption />
-  </S.Wrapper>
+const NavigationMore = ({ open, handleClose, anchorEl }: any) => (
+  <GS.StyledMenu
+    id="navigation-more"
+    open={open}
+    anchorEl={anchorEl}
+    onClose={handleClose}
+  >
+    <S.Wrapper>
+      <S.TopMenuSectionWrapper>
+        <S.TopMenu>
+          {local.MENU_OPTIONS &&
+            local.MENU_OPTIONS.map((item, index) => (
+              <MenuItem key={`${item.name + index}`} onClick={handleClose}>
+                <CustomLink to={item.link} label={item.name} />
+              </MenuItem>
+            ))}
+        </S.TopMenu>
+      </S.TopMenuSectionWrapper>
+      <Divider variant="middle" />
+      <S.BottomMenuSectionWrapper>
+        <MenuItem onClick={handleClose}>
+          <SettingsOption />
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <LogoutOption />
+        </MenuItem>
+      </S.BottomMenuSectionWrapper>
+    </S.Wrapper>
+  </GS.StyledMenu>
 )
 
 export default NavigationMore
