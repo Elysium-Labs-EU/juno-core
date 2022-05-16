690f1b5fe6df01b7b5b777ef32355e44547ebf71
Robbert <robberttg@protonmail.com>
2022-05-16 19:55:12 +0200

    Updates

--- src/utils/handleUserTokens.ts
+++ src/utils/handleUserTokens.ts
@@ -5,7 +5,7 @@ import removeCookie from './Cookie/removeCookie'
 const handleUserTokens = (response?: any) => ({
   setBothTokens: () => {
     setCookie(global.SESSION_TOKEN, response.data.access_token, 30)
-    localStorage.setItem(global.REFRESH_TOKEN, response.data.refresh_token)
+    // localStorage.setItem(global.REFRESH_TOKEN, response.data.refresh_token)
   },
   setAccessToken: () => {
     setCookie(global.SESSION_TOKEN, response.data.access_token, 30)
