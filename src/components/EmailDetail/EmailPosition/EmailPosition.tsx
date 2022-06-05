7de54374729c4aea09e023e2321c082b36267e57
Robbert <robberttg@protonmail.com>
2022-06-05 12:57:07 +0200

    Tweak loader

--- src/components/EmailDetail/EmailPosition/EmailPosition.tsx
+++ src/components/EmailDetail/EmailPosition/EmailPosition.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react'
-import CircularProgress from '@mui/material/CircularProgress'
 import Tooltip from '@mui/material/Tooltip'
 import { FiAlertCircle } from 'react-icons/fi'
 import { selectSessionViewIndex } from '../../../Store/emailDetailSlice'
@@ -9,6 +8,7 @@ import * as GS from '../../../styles/globalStyles'
 import * as global from '../../../constants/globalConstants'
 import labelApi from '../../../data/labelApi'
 import { selectLabelIds } from '../../../Store/labelsSlice'
+import StyledCircularProgress from '../../Elements/StyledCircularProgress'
 
 const EXPLANATION =
   'This shows on what email you are of all the emails in this GMail box.'
@@ -50,7 +50,7 @@ const EmailPosition = () => {
         </Tooltip>
       )}
       {loadingState === global.LOAD_STATE_MAP.loading && (
-        <CircularProgress size={10} />
+        <StyledCircularProgress size={10} />
       )}
       {loadingState === global.LOAD_STATE_MAP.error && <FiAlertCircle />}
     </S.Wrapper>
