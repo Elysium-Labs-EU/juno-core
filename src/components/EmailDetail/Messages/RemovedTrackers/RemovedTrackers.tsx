c44e3ab2a2810d6d64492991fa7ef571372beeab
Robbert <robberttg@protonmail.com>
2022-06-14 23:25:50 +0200

    Enhance Removed Trackers

--- src/components/EmailDetail/Messages/RemovedTrackers/RemovedTrackers.tsx
+++ src/components/EmailDetail/Messages/RemovedTrackers/RemovedTrackers.tsx
@@ -1,14 +1,21 @@
 import { FiShield } from 'react-icons/fi'
-import { SmallTextMuted } from '../../EmailDetailStyles'
+import { Tooltip } from '@mui/material'
 import * as S from './RemovedTrackersStyles'
+import * as GS from '../../../../styles/globalStyles'
+import * as themeConstants from '../../../../constants/themeConstants'
 
 const REMOVED_TRACKERS = 'Trackers removed'
+const JUNO_TRACKERS = 'Juno has blocked trackers on the email.'
 
 const RemovedTrackers = () => (
   <S.Wrapper>
     <S.Inner>
-      <FiShield size={10} />
-      <SmallTextMuted>{REMOVED_TRACKERS}</SmallTextMuted>
+      <S.Icon>
+        <FiShield size={10} color={themeConstants.colorGrey} />
+      </S.Icon>
+      <Tooltip title={JUNO_TRACKERS}>
+        <GS.TextSpanSmall>{REMOVED_TRACKERS}</GS.TextSpanSmall>
+      </Tooltip>
     </S.Inner>
   </S.Wrapper>
 )
--- src/components/EmailDetail/Messages/RemovedTrackers/RemovedTrackersStyles.ts
+++ src/components/EmailDetail/Messages/RemovedTrackers/RemovedTrackersStyles.ts
@@ -10,4 +10,11 @@ export const Inner = styled.div`
   align-items: center;
   display: flex;
   padding-bottom: 16px;
+  cursor: default;
+`
+
+export const Icon = styled.div`
+  line-height: 0;
+  text-align: center;
+  margin-right: 4px;
 `
