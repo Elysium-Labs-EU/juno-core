c3bfcd9bb57b07768d617618e088a94f55388f68
Robbert <robberttg@protonmail.com>
2022-06-28 08:13:00 +0200

    Update api.ts

--- src/data/api.ts
+++ src/data/api.ts
@@ -9,6 +9,8 @@ assertNonNullish(
   'Unable to find API key in environment'
 )
 
+// BLA
+
 export const BASE_API_URL = import.meta.env.VITE_BACKEND_URL.replace(/\/$/, '')
 
 export const fetchToken = () => {
