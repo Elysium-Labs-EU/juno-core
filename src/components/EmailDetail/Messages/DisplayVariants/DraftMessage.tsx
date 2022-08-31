3b8e6aa3c3b91bab17305f04e893b7422c28f445
Robbert <robberttg@protonmail.com>
2022-08-31 20:54:23 +0200

    Add discard draft function inline

--- src/components/EmailDetail/Messages/DisplayVariants/DraftMessage.tsx
+++ src/components/EmailDetail/Messages/DisplayVariants/DraftMessage.tsx
@@ -1,14 +1,24 @@
-import { useMemo } from 'react'
+import { useCallback, useMemo, useState } from 'react'
+import { FiEdit, FiDelete } from 'react-icons/fi'
+import * as GS from '../../../../styles/globalStyles'
 import EmailAvatar from '../../../Elements/Avatar/EmailAvatar'
 import TimeStamp from '../../../Elements/TimeStamp/TimeStampDisplay'
 import * as local from '../../../../constants/draftConstants'
+import * as global from '../../../../constants/globalConstants'
 import * as S from '../../EmailDetailStyles'
 import { selectCurrentEmail } from '../../../../store/emailDetailSlice'
-import { useAppSelector } from '../../../../store/hooks'
+import { useAppDispatch, useAppSelector } from '../../../../store/hooks'
 import { IEmailMessage } from '../../../../store/storeTypes/emailListTypes'
 import SenderNameFull from '../../../Elements/SenderName/senderNameFull'
 import SenderNamePartial from '../../../Elements/SenderName/senderNamePartial'
 import { selectProfile } from '../../../../store/baseSlice'
+import EmailSubject from '../../../Elements/EmailSubject'
+import EmailHasAttachment from '../../../Elements/EmailHasAttachment'
+import EmailDetailBody from '../EmailDetailBody/EmailDetailBody'
+import LinkedContacts from './Recipients/LinkedContacts'
+import CustomButton from '../../../Elements/Buttons/CustomButton'
+import { selectDraft } from '../../../../store/draftsSlice'
+import discardDraft from '../../../EmailOptions/DiscardDraft'
 
 const DraftMessage = ({
   message,
@@ -25,12 +35,14 @@ const DraftMessage = ({
   ) => void
   hideDraft: boolean
 }) => {
+  const [open, setOpen] = useState<boolean>(true)
   const id = useAppSelector(selectCurrentEmail)
   const { emailAddress } = useAppSelector(selectProfile)
-  const messageId = message && message.id
+  const draftList = useAppSelector(selectDraft)
+  const dispatch = useAppDispatch()
 
   const EmailSnippet =
-    message && `${message.snippet.replace(/^(.{65}[^\s]*).*/, '$1')}...`
+    message && `${ message.snippet.replace(/^(.{65}[^\s]*).*/, '$1') }...`
 
   const staticSenderNameFull = useMemo(
     () => SenderNameFull(message.payload.headers?.from, emailAddress),
@@ -41,17 +53,44 @@ const DraftMessage = ({
     []
   )
 
+  const staticEmailSubject = useMemo(
+    () => EmailSubject(message.payload.headers?.subject),
+    []
+  )
+
   // Send back detail to the parent component to hide the draft from the list and open the composer
-  const handleClickLocal = () => {
-    handleClickListener(id, messageId, draftIndex)
+  const handleEditLocal = () => {
+    handleClickListener(id, message?.id, draftIndex)
   }
 
-  return (
+  const handleOpenClose = () => {
+    setOpen((currState) => !currState)
+  }
+
+  const draftMessage = useCallback(
+    () => draftList.find((draft) => draft.message.id === message.id),
+    [message]
+  )
+
+  const handleDiscardDraft = useCallback(() => {
+    const foundDraft = draftMessage()
+    if (foundDraft) {
+      discardDraft({
+        messageId: foundDraft.message.id,
+        threadId: foundDraft.message.threadId,
+        dispatch,
+        draftId: foundDraft.id,
+      })
+    }
+  }, [message])
+
+  return !open ? (
     <S.EmailClosedWrapper
-      onClick={handleClickLocal}
+      onClick={handleOpenClose}
       aria-hidden="true"
       hideDraft={hideDraft}
       isDraft
+      title="Click to open"
     >
       <S.ClosedMessageWrapper>
         <S.TopContainer>
@@ -78,6 +117,62 @@ const DraftMessage = ({
         </S.TimeAttachmentContainer>
       </S.ClosedMessageWrapper>
     </S.EmailClosedWrapper>
+  ) : (
+    <S.EmailOpenWrapper isDraft hideDraft={hideDraft}>
+      <S.DraftHeaderControls>
+        <GS.TextMutedSmall style={{ marginRight: '40px' }}>
+          {local.DRAFT_INDICATOR_HEADER}
+        </GS.TextMutedSmall>
+        <CustomButton
+          style={{ marginRight: '10px' }}
+          label="Edit"
+          title="Edit the draft"
+          icon={<FiEdit />}
+          suppressed
+          onClick={handleEditLocal}
+        />
+        <CustomButton
+          label="Discard"
+          title="Discard the draft"
+          icon={<FiDelete />}
+          suppressed
+          onClick={handleDiscardDraft}
+        />
+      </S.DraftHeaderControls>
+      <div
+        onClick={handleOpenClose}
+        title="Click to close"
+        style={{ cursor: 'pointer' }}
+        aria-hidden="true"
+      >
+        <S.TopContainer>
+          <S.HeaderFullWidth>
+            <EmailAvatar avatarURL={staticSenderNameFull} />
+            <S.EmailDetailTitle title={staticEmailSubject}>
+              {staticEmailSubject}
+            </S.EmailDetailTitle>
+            <S.TimeAttachmentContainer>
+              <S.ChildDiv>
+                <EmailHasAttachment messages={message} />
+              </S.ChildDiv>
+              <S.ChildDiv>
+                <TimeStamp threadTimeStamp={message.internalDate} />
+              </S.ChildDiv>
+            </S.TimeAttachmentContainer>
+          </S.HeaderFullWidth>
+        </S.TopContainer>
+        <LinkedContacts message={message} />
+        <S.GreyDivider />
+        <S.EmailBody>
+          {message && message?.payload && message?.id && (
+            <EmailDetailBody
+              threadDetailBody={message.payload}
+              detailBodyCSS={global.EMAIL_BODY_VISIBLE}
+            />
+          )}
+        </S.EmailBody>
+      </div>
+    </S.EmailOpenWrapper>
   )
 }
 
