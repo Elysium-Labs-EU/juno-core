b8f5d03c4f1b9d856812626935130d1d09bf9a6d
Robbert <robberttg@protonmail.com>
2022-09-08 21:57:58 +0200

    Hot fix

--- src/components/EmailDetail/EmailDetailHeader.tsx
+++ src/components/EmailDetail/EmailDetailHeader.tsx
@@ -40,8 +40,8 @@ const EmailDetailHeader = ({
     if (storageLabels.length > 0 && labelIds.length > 0) {
       if (location?.pathname?.includes(labelIds[0])) {
         const matchedLabel = findLabelById({ storageLabels, labelIds })
-        if (matchedLabel.length > 0) {
-          const splitHeader = matchedLabel[0].name.split('/')
+        if (matchedLabel) {
+          const splitHeader = matchedLabel.name.split('/')
           mounted &&
             setDetailHeader(splitHeader[splitHeader.length - 1].toLowerCase())
         } else {
