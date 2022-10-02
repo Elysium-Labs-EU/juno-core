c369460b8888467c01c06588a76f6d71bf73e3b3
Robbert Tuerlings <robberttg@protonmail.com>
2022-10-02 20:42:11 +0200

    Update UnsubscribeOption.tsx

--- src/components/EmailDetail/Options/UnsubscribeOption.tsx
+++ src/components/EmailDetail/Options/UnsubscribeOption.tsx
@@ -5,7 +5,7 @@ import useMultiKeyPress from '../../../hooks/useMultiKeyPress'
 import { QiMailUnsub } from '../../../images/svgIcons/quillIcons'
 import { useAppSelector } from '../../../store/hooks'
 import { selectInSearch } from '../../../store/utilsSlice'
-import createComposeViaURL from '../../../utils/createComposeViaUrl'
+import createComposeViaURL from '../../../utils/createComposeViaURL'
 import { setModifierKey } from '../../../utils/setModifierKey'
 import CustomButton from '../../Elements/Buttons/CustomButton'
 
