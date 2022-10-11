6d98e426813306b9f36b8701cf5a9c41eb9c0998
ibukunolouwa oyeniyi <ibukun46@gmail.com>
2022-10-11 14:15:40 +0100

    added timestamp as title tag

--- src/components/Archive/ArchiveHeader.tsx
+++ src/components/Archive/ArchiveHeader.tsx
@@ -1,18 +1,32 @@
+import format from 'date-fns/format'
 import Navigation from '../MainHeader/Navigation/Navigation'
 import * as S from '../MainHeader/HeaderStyles'
 import * as GS from '../../styles/globalStyles'
+import { useAppSelector } from '../../store/hooks'
+import { selectIsFlexibleFlowActive } from '../../store/utilsSlice'
+import { selectEmailList,selectActiveEmailListIndex } from '../../store/emailListSlice'
 
 const ARCHIVE_HEADER = 'Archive'
 
-const AllEmailHeader = () => (
-  <GS.OuterContainer>
-    <S.NavContainer>
-      <S.HeaderCenter>
-        <S.PageTitle>{ARCHIVE_HEADER}</S.PageTitle>
-      </S.HeaderCenter>
-      <Navigation />
-    </S.NavContainer>
-  </GS.OuterContainer>
-)
+const AllEmailHeader = () => {
+  const emailList = useAppSelector(selectEmailList)
+  const activeEmailListIndex = useAppSelector(selectActiveEmailListIndex)
+  const timeStamp = emailList[activeEmailListIndex]?.timestamp 
+  let unixTimeStamp
+  if (timeStamp !== undefined){
+  unixTimeStamp = format(timeStamp,"dd mm yyyy")
+  }
+  return (
+    <GS.OuterContainer>
+      <S.NavContainer>
+        <S.HeaderCenter>
+          <S.PageTitle title={unixTimeStamp}>{ARCHIVE_HEADER}</S.PageTitle>
+        </S.HeaderCenter>
+        <Navigation />
+      </S.NavContainer>
+    </GS.OuterContainer>
+  )
+}
+
 
 export default AllEmailHeader
